220f1448a05e95bc9ff564da10ec401a4e5a4977
SashaLyapanov <lyapanov_s@mail.ru>
2026-05-07 19:06:43 +0300

    Fix screen to JPA Entity.

--- src/main/java/h2o/tz/lyapanov/tz_lyapanov_aa/model/Screen.java
+++ src/main/java/h2o/tz/lyapanov/tz_lyapanov_aa/model/Screen.java
@@ -2,12 +2,14 @@ package h2o.tz.lyapanov.tz_lyapanov_aa.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Entity
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Table(name = "screens")
 public class Screen extends Device{
